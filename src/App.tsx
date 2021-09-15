56dafb9df22894ed15e86c8e39fb660abaaf4595
Sam Aryasa <sam@stashaway.com>
2021-09-15 14:02:13 +0800

    feat: Add working prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import formatEvidences from "./formatEvidences";
 
 function App() {
+  const [content, setContent] = useState("");
+
+  const format = useCallback(() => {
+    const formattedContent = formatEvidences(content);
+
+    setContent(formattedContent);
+    navigator.clipboard.writeText(formattedContent);
+  }, [content, setContent]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: 100 }}>
+      <h1>Evidence formatters</h1>
+
+      <textarea
+        style={{ height: 300, width: 400 }}
+        value={content}
+        onChange={event => setContent(event.target.value)}
+      />
+
+      <button style={{ display: "block" }} onClick={format}>
+        Format & copy
+      </button>
     </div>
   );
 }
